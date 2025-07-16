ec49c55765521717457532d3a2300fce85aca94b
Nick Sansalone <73195645+nixksans@users.noreply.github.com>
2025-07-16 17:02:40 -0400

    Add Telescope fzf extension

--- init.lua
+++ init.lua
@@ -39,9 +39,27 @@ require("lazy").setup({
   },
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        config = function()
+          require("telescope").load_extension("fzf")
+        end,
+      },
+    },
     config = function()
-      require("telescope").setup({})
+      require("telescope").setup({
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+          },
+        },
+      })
       vim.keymap.set(
         "n",
         "<leader>f",
