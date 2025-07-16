e955e2a9fe2b79e72a57ca69095a85d02df2ef91
Nick Sansalone <73195645+nixksans@users.noreply.github.com>
2025-07-16 12:55:26 -0400

    Add nvim-tree file explorer with icons

--- ReadMe.md
+++ ReadMe.md
@@ -1,7 +1,12 @@
 # Nicks NeoVim Setup
 
-## Started with 
+## Started with
 ```git clone https://github.com/nvim-lua/kickstart.nvim ~/.config/nvim```
 
 ## Install the Plugin Manager lazy.nvim:
 ```git clone https://github.com/folke/lazy.nvim.git ~/.local/share/nvim/site/pack/lazy/start/lazy.nvim```
+
+## File Explorer
+The configuration uses **nvim-tree.lua** for browsing files.
+Toggle it with `<leader>e` (space + e). Files are displayed with icons thanks to **nvim-web-devicons**.
+
--- init.lua
+++ init.lua
@@ -12,9 +12,26 @@ vim.opt.termguicolors = true
 vim.opt.mouse = "a"
 vim.opt.clipboard = "unnamedplus"
 
+-- Leader Key
+vim.g.mapleader = " "
+
 -- Plugin Manager Bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 vim.opt.rtp:prepend(lazypath)
 
 -- Plugin Setup
-require("lazy").setup()
+require("lazy").setup({
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup()
+      vim.keymap.set(
+        "n",
+        "<leader>e",
+        "<cmd>NvimTreeToggle<CR>",
+        { silent = true, desc = "Toggle file explorer" }
+      )
+    end,
+  },
+})
