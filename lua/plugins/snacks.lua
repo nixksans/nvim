3630cdfd034fc5c70de8645a72da3e8a6e19e426
nixksans <nicksans10@gmail.com>
2025-07-17 19:46:36 -0400

    Added a welcome page

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -21,6 +21,15 @@ return {
     explorer = {
       enabled = true,
     },
+    dashboard = {
+      enabled = true,
+      sections = {
+        { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
+    },
   },   
   config = function(_, opts)
     local snacks = require("snacks")
