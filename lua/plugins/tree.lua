aafffcd566d63850384405759c3869487b15c124
Nick Sansalone <73195645+nixksans@users.noreply.github.com>
2025-07-17 14:44:15 -0400

    Enable single-click open in nvim-tree

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -2,7 +2,21 @@ return {
   "nvim-tree/nvim-tree.lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("nvim-tree").setup()
+    local function on_attach(bufnr)
+      local api = require("nvim-tree.api")
+      api.config.mappings.default_on_attach(bufnr)
+      vim.keymap.set(
+        "n",
+        "<LeftRelease>",
+        api.node.open.edit,
+        { buffer = bufnr, desc = "Open file or folder" }
+      )
+    end
+
+    require("nvim-tree").setup({
+      on_attach = on_attach,
+    })
+
     vim.keymap.set(
       "n",
       "<leader>e",
