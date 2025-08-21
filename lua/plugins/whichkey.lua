bae2532091a7bf1c2528c9da4686048f7537ce0a
Nick Sansalone <73195645+nixksans@users.noreply.github.com>
2025-08-21 16:39:54 -0400

    fix: keep which-key menu open

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -2,6 +2,7 @@ return {
   "folke/which-key.nvim",
   lazy = false,
   config = function()
+    vim.o.timeout = false
     local wk = require("which-key")
     wk.setup({
       triggers = { "<leader>" },
